d2b10319b526c9f920aeb74ea709ec093d021b43
dzmitry <dzmitry.zhukau@qweeco.com>
2015-07-15 10:49:16 +0300

    Refactor setup dependencies

--- setup.py
+++ setup.py
@@ -8,9 +8,6 @@ setup(
     include_package_data=True,
     install_requires=[
         'tornado==4.2',
-        'motor==0.4.1',
-        'jmespath==0.7.1',
-        'mock==1.0.1',
         'pycrypto==2.6.1',
     ],
 )
