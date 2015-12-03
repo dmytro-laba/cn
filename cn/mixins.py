43c0beba488bb79de613a071e84db1b3163e44d2
Valery Satsura <valera@qweeco.com>
2015-12-03 15:34:26 +0300

    removed coroutine code

--- cn/mixins.py
+++ cn/mixins.py
@@ -9,8 +9,3 @@ class AsyncClientMixin(object):
 
     def get_user_cipher(self, user_id, keys_dir, aws_access_key=None, aws_secret_key=None, aws_bucket=None):
         return UserAESCipher(user_id, keys_dir, aws_access_key, aws_secret_key, aws_bucket)
-
-    @gen.coroutine
-    def get_async_user_cipher(self, user_id, keys_dir, aws_access_key=None, aws_secret_key=None, aws_bucket=None):
-        obj = yield AsyncUserAESCipher(user_id, keys_dir, aws_access_key, aws_secret_key, aws_bucket)
-        return obj
