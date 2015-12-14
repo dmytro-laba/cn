dbe2241670b68847e343daf9150fb0f62a7837f9
Aleh Rudzko <aleh.rudzko@qweeco.com>
2015-12-14 08:20:32 +0300

    fix

--- cn/decorators.py
+++ cn/decorators.py
@@ -7,22 +7,20 @@ def authentication(url_for_check):
         @tornado.gen.coroutine
         def wrapper(self, **kwargs):
             http_client = self.get_async_http_client()
+            try:
+                auth_params = auth_headers_parse(self.request.headers)
+            except KeyError as e:
+                raise tornado.web.HTTPError(400, '%s is required' % str(e))
 
-            # try:
-            #     auth_params = auth_headers_parse(self.request.headers)
-            # except KeyError as e:
-            #     raise tornado.web.HTTPError(400, '%s is required' % str(e))
-            #
-            # try:
-            #     url = tornado.httputil.url_concat(url_for_check, auth_params)
-            #     print('REQUEST: %s' % url)
-            #     response = yield http_client.fetch(url,
-            #                                        connect_timeout=float('inf'),
-            #                                        request_timeout=float('inf'))
-            #     return func(self, **kwargs)
-            # except tornado.httpclient.HTTPError as e:
-            #     print('Exception:%s' % url)
-            #     raise tornado.web.HTTPError(e.code, 'Token not valid')
-            return func(self, **kwargs)
+            url = tornado.httputil.url_concat(url_for_check, auth_params)
+            try:
+                response = yield http_client.fetch(url,
+                                                   connect_timeout=float('inf'),
+                                                   request_timeout=float('inf'))
+                return func(self, **kwargs)
+            except tornado.httpclient.HTTPError as e:
+                print('Exception:%s' % url)
+                raise tornado.web.HTTPError(e.code, 'Token not valid')
+            #return func(self, **kwargs)
         return wrapper
     return decorator
